227c97b8e2921495c326908e3b19613b8e60e837
skepth <sadenithin.cs@gmail.com>
2023-09-28 22:07:08 -0700

    Logic to place mines randomly

--- src/board.rs
+++ src/board.rs
@@ -1,7 +1,12 @@
 // The board.rs implements the tilemap/game board for minesweeper.
 
 use cli_table::Table;
-use std::fmt;
+use colored::Colorize;
+use rand::{distributions::Uniform, Rng};
+use std::{collections::HashSet, fmt};
+
+const SIZE_OF_BOARD: usize = 4;
+const NO_OF_MINES: usize = 4;
 
 // RenderCoordinates hold the bottom left position relative to the game window.
 #[derive(Debug, Clone)]
@@ -38,16 +43,33 @@ impl BoardCell {
 }
 
 impl Board {
+    // Constructor for the board sets the game objects & initializes the game.
     pub fn new() -> Self {
-        Board(vec![vec![BoardCell::new(); 4]; 4])
+        Board(vec![vec![BoardCell::new(); SIZE_OF_BOARD]; SIZE_OF_BOARD]).place_mines(NO_OF_MINES)
+    }
+
+    // Chooses random cells to place the mines on the board.
+    fn place_mines(mut self, no_of_mines: usize) -> Self {
+        let mut random_coord: HashSet<(usize, usize)> = HashSet::new();
+        let mut rng = rand::thread_rng();
+        let range = Uniform::new(0, SIZE_OF_BOARD - 1);
+
+        while random_coord.len() < no_of_mines {
+            let coord = (rng.sample(&range), rng.sample(&range));
+            if random_coord.insert(coord) {
+                self.0[coord.0][coord.1].cell_type = GameObject::MINE;
+            }
+        }
+        println!("Log: hashsets => {:?}", random_coord);
+        self
     }
 }
 
 impl fmt::Debug for GameObject {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Self::MINE => write!(f, "*"),
-            Self::NEIGHBOUR => write!(f, "N"),
+            Self::MINE => write!(f, "{}", "*".red().bold()),
+            Self::NEIGHBOUR => write!(f, "{}", "N".blue()),
             Self::EMPTY => write!(f, " "),
         }
     }
